52e41e88dd512d5e22285a824609df26b644d4b9
kweonminsung <kms@DESKTOP-J9EJ17G.localdomain>
2021-04-13 12:56:00 +0900

    app 파일 변경

--- app/app.js
+++ app/app.js
@@ -7,11 +7,11 @@ const app = express();
 
 
 // 라우팅
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 // 앱 세팅
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 
