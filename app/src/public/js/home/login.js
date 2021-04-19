ac58ef3392c28f68ff8d4b3803232583ca888ab8
kweonminsung <79230043+kweonminsung@users.noreply.github.com>
2021-04-19 17:40:04 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,7 +19,9 @@ const id = document.querySelector("#id"),
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(req),
-        });
+        })
+        .then((res) => res.json())
+        .then(console.log);
     }
 
 
